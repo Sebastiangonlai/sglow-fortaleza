90773ab335ad6b7fa0a8e5cfb5ccdc0444f31723
Sebastiangonlai <58099324+Sebastiangonlai@users.noreply.github.com>
2025-04-22 00:13:05 -0300

    develop-

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import React from "react";
 // import NavBar from '@/components/common/NavBar';
-// import Footer from '@/components/common/Footer';
+// // import Footer from '@/components/common/Footer';
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx";
 
@@ -11,9 +11,9 @@ import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <NavBar /> */}
+      <NavBar />
       <App />
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   </React.StrictMode>
 );
