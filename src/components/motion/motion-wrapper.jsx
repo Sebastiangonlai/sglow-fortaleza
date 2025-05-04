d14b6d587f469bfb3e26ab5d435d32d84803bda2
Sebastiangonlai <58099324+Sebastiangonlai@users.noreply.github.com>
2025-05-04 00:06:26 -0300

    develop-

--- src/components/motion/motion-wrapper.jsx
+++ src/components/motion/motion-wrapper.jsx
@@ -29,8 +29,8 @@ export const MotionWrapper = ({
     slide: {
       hidden: {
         opacity: 0,
-        y: direction === "up" ? 50 : direction === "down" ? -50 : 0,
-        x: direction === "left" ? 50 : direction === "right" ? -50 : 0,
+        y: direction === "up" ? 10 : direction === "down" ? -10 : 0,
+        x: direction === "left" ? 10 : direction === "right" ? -10 : 0,
       },
       visible: {
         opacity: 1, x: 0, y: 0,
